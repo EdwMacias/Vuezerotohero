17c946d357cd66b539640e9b71ff48840eb2db89
EdwMacias <edwardavid26@hotmail.com>
2024-07-06 08:48:40 -0500

    codigo de promises extenso antes de limpieza

--- 01-bases/src/bases/07-imp-exp.ts
+++ 01-bases/src/bases/07-imp-exp.ts
@@ -1,16 +1,16 @@
 import heroes, { type Owner } from "../data/heroes";
 
- console.log( heroes)
+//  console.log( heroes)
 
 export const getHeroById = (id: number) => {
-    return heroes.find( h => h.id === id) ?? {};
+    return heroes.find( h => h.id === id) ;
 }
 
-console.log(getHeroById(1))
+// console.log(getHeroById(1))
 
 export const getHeroesByOwner = (owner: Owner) => {
     return heroes.filter( h => h.owner === owner);
 }
 
-console.log(getHeroesByOwner('Marvel'));
+// console.log(getHeroesByOwner('Marvel'));
 
--- 01-bases/src/bases/08-promises.ts
+++ 01-bases/src/bases/08-promises.ts
@@ -1,16 +1,43 @@
-console.log('inicio')
+import { getHeroById } from "./07-imp-exp";
+import type { Hero } from "../data/heroes";
+// console.log('inicio')
 
 
 
 
-new Promise( (resolve) => {
-    // console.log('Cargando...')
-    resolve('Mi amigo cumplio')
-}).then((message) => {
-    console.log(message)
+// new Promise( (resolve, reject) => {
+//     // console.log('Cargando...')
+//    setTimeout(() => {
+//     //  resolve('Mi amigo cumplio')
+
+//      reject('No se pudo cargar')
+   
+//     },1000);
+//    return;
+// }).then((message) => {
+//     console.log(message)
+// })
+// .catch(error => console.warn(error))
+// .finally(() => console.log('Finalizo'))
+
+
+
+// console.log('fin')
+
+const getHeroByIdAsync = (id: number):Promise<Hero> =>{
+    return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+        const hero = getHeroById(id);
+        if (hero) {
+            resolve(hero);
+        } else {
+            reject(`No se pudo encontrar el héroe ${id}`);
+        }
+    }, 1500);
+
 })
+}
 
-
-
-
-console.log('fin')
+getHeroByIdAsync(200)
+    .then(hero => console.log('el nombre es: ', hero.name))
+    .catch(alert);
