3a1c33bb47cc8525ed7c7c4648d828242661766a
EdwMacias <edwardavid26@hotmail.com>
2024-08-25 08:33:15 -0500

    test de counter y squared counter

--- Vue/03-indecision-app/src/components/MyCounter.vue
+++ Vue/03-indecision-app/src/components/MyCounter.vue
@@ -1,12 +1,9 @@
 <template>
     <section>
         <h3>Counter: {{ counter }}</h3>
-        <h3>Square: {{ squaredCounter }}</h3>
-
+        <h3 data-testid="square-label">Square: {{ squaredCounter }}</h3>
         <div>
-            <button class="btn"  @:click="increment">+1</button>
-            <button class="btn" @:click="decrement">-1</button>
-            <button class="btn" @:click="reset">Reset</button>
+            <button class="btn" @:click="counter--">-1</button>
         </div>
     </section>
 </template>
@@ -14,13 +11,18 @@
 
 <script lang="ts" setup>
 import { useCounter } from '../composables/useCounter';
-const {counter, squaredCounter, increment, decrement, reset} = useCounter();
+
+interface Props {
+    value: number;
+}
+const props = defineProps<Props>();
+const { counter, squaredCounter } = useCounter(props.value);
 
 </script>
 
 
-<style>
-.btn{
+<style scoped>
+.btn {
     @apply p-5 rounded bg-blue-200 hover:bg-blue-700 mr-2;
 }
 </style>
--- Vue/03-indecision-app/tests/helpers/sum.test.ts
+++ Vue/03-indecision-app/tests/helpers/sum.test.ts
@@ -1,9 +1,24 @@
 // sum.test.js
-import { expect, test } from 'vitest';
-import { sum } from '../../src/helpers/sum';
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-  // if (sum(1, 2) !== 3) {
-  //   throw new Error('test failed');
-  // }
+import { describe, expect, test } from 'vitest';
+import { sum, addArray } from '../../src/helpers/sum';
+
+describe('add function', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
 });
+
+describe('addArray function', () => {
+  test('should return 0 if the array is empty', () => {
+    const numberArray: number[] = [];
+    const result = addArray(numberArray);
+    expect(result).toBe(0);
+  });
+  
+  test('should return the proper value of the addArray function', () => {
+    const numberArray = [1, 2, 3, 4, 5];
+    const result = addArray(numberArray);
+    expect(result).toBe(15);
+  });
+});
+
